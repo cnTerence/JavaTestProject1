1493d0190d6c4a31b56508a510608ab216443923
cnTerence <cn_terence@163.com>
2014-09-26 17:54:24 +0800

    update sth

--- src/vargas/dgsd22/TestClass1.java
+++ src/vargas/dgsd22/TestClass1.java
@@ -17,44 +17,44 @@ public class TestClass1 {
 		PropertyManagerExpert.init();
 		PropertyManagerMatch.init();
 		
-		Map<String, Expert> lastPubRec = new HashMap<String, Expert>();
-		ExpertFetcher fetcher = new ExpertFetcher();
-		List<Expert> newRec = fetcher.getAllPojo(lastPubRec);
-		
-		Iterator<String> keySetIterator = lastPubRec.keySet().iterator();  
-		while (keySetIterator.hasNext()) {  
-			String key = keySetIterator.next();  
-			Expert value = lastPubRec.get(key);
-			
-			System.out.println(value.getUid() + " | " +
-					value.getUname() + " | " +
-					value.getDate() + " | " +
-					value.getType() + " | " +
-					value.getHost() + " | " +
-					value.getGuest() + " | " +
-					value.isResult() + " | " +
-					value.getP());
-		}  
-		
-		System.out.println("----------------------------------");
-		
-		int len = newRec.size();
-		String[] output = new String[len];
-		
-		int cnt;
-		for(cnt = 0; cnt < len; cnt++){
-			output[cnt] = newRec.get(cnt).getUid() + " | " +
-					newRec.get(cnt).getUname() + " | " +
-					newRec.get(cnt).getDate() + " | " +
-					newRec.get(cnt).getType() + " | " +
-					newRec.get(cnt).getHost() + " | " +
-					newRec.get(cnt).getGuest() + " | " +
-					newRec.get(cnt).isResult() + " | " +
-					newRec.get(cnt).getP();
-			
-			System.out.println(output[cnt]);
-					
-		}
+//		Map<String, Expert> lastPubRec = new HashMap<String, Expert>();
+//		ExpertFetcher fetcher = new ExpertFetcher();
+//		List<Expert> newRec = fetcher.getAllPojo(lastPubRec);
+//		
+//		Iterator<String> keySetIterator = lastPubRec.keySet().iterator();  
+//		while (keySetIterator.hasNext()) {  
+//			String key = keySetIterator.next();  
+//			Expert value = lastPubRec.get(key);
+//			
+//			System.out.println(value.getUid() + " | " +
+//					value.getUname() + " | " +
+//					value.getDate() + " | " +
+//					value.getType() + " | " +
+//					value.getHost() + " | " +
+//					value.getGuest() + " | " +
+//					value.isResult() + " | " +
+//					value.getP());
+//		}  
+//		
+//		System.out.println("----------------------------------");
+//		
+//		int len = newRec.size();
+//		String[] output = new String[len];
+//		
+//		int cnt;
+//		for(cnt = 0; cnt < len; cnt++){
+//			output[cnt] = newRec.get(cnt).getUid() + " | " +
+//					newRec.get(cnt).getUname() + " | " +
+//					newRec.get(cnt).getDate() + " | " +
+//					newRec.get(cnt).getType() + " | " +
+//					newRec.get(cnt).getHost() + " | " +
+//					newRec.get(cnt).getGuest() + " | " +
+//					newRec.get(cnt).isResult() + " | " +
+//					newRec.get(cnt).getP();
+//			
+//			System.out.println(output[cnt]);
+//					
+//		}
 		
 		
 	}
