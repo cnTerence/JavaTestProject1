01ad00ef9a4708733699ee7e547157d9da3b3308
terence <terence@terence-PC>
2014-10-11 00:55:06 +0800

    update

--- src/vargas/dgsd22/fetcher/ExpertFetcher.java
+++ src/vargas/dgsd22/fetcher/ExpertFetcher.java
@@ -15,74 +15,78 @@ import vargas.dgsd22.prop.PropertyManagerExpert;
 
 public class ExpertFetcher extends Fetcher {
 
-	public List<Expert> getAllPojo() {
+	public List<Expert> getAllPojo()  {
 		
 		List<Expert> expertList = new ArrayList<Expert>();
 		
 		Map<String, String> expertId = PropertyManagerExpert.getAllExperts();
 		int len2;
 		int cnt2;
-		String uid;
+		String uid = null;
 		String uname;
 		List<String> usedUid = new ArrayList<String>();
 		
-		Iterator<String> keySetIterator = expertId.keySet().iterator();  
-		while (keySetIterator.hasNext()) {  
-			uid = keySetIterator.next();
-			
-			//check if it is the duplicate uid
-			if(usedUid.contains(uid)){
-				continue;
-			}
-			
-			Document doc = getDocument(Url.getExpertSite(uid));
-			
-			uname = 
-					doc.getElementsByAttributeValue(HtmlHelper.CLASS, HtmlHelper.getExpertNameDivClass1()).get(0).
-					getElementsByAttributeValue(HtmlHelper.CLASS, HtmlHelper.getExpertNameDivClass2()).
-					get(0).child(0).child(0).text();
-			
-			Element tbl = 
-					doc.getElementsByAttributeValue(
-							HtmlHelper.CLASS, 
-							HtmlHelper.getExpertResultDivClass()).get(0).
-							getElementsByTag(HtmlHelper.TABLE).get(1).child(0);
-			
-			len2 = tbl.children().size();
-			
-			Element row = null;
-			Expert expert = null;
-			
-			cnt2 = len2 - 1;
-			for(cnt2 = len2 - 1; cnt2 >= 0; cnt2--){
-				row = tbl.child(cnt2);
+//		try{
+			Iterator<String> keySetIterator = expertId.keySet().iterator();  
+			while (keySetIterator.hasNext()) {  
+				uid = keySetIterator.next();
 				
-				if(row.child(5).children().size() == 0){
+				//check if it is the duplicate uid
+				if(usedUid.contains(uid)){
 					continue;
 				}
 				
-				expert = new Expert();
-				expert.setUid(uid);
-				expert.setUname(uname);
-				expert.setType(row.child(1).child(0).text());
-				expert.setDate(row.child(2).child(0).text());
-				expert.setHost(row.child(3).child(0).child(0).child(0).text());
-				expert.setGuest(row.child(3).child(0).child(0).child(2).text());
-				expert.setP(row.child(4).text());
-				String t = row.child(5).child(0).attr(HtmlHelper.SRC);
-				if(t.contains(HtmlHelper.getExpertWinImgName())){
-					expert.setResult(true);
-				}else if(t.contains(HtmlHelper.getExpertLostImgName())){
-					expert.setResult(false);
+				Document doc = getDocument(Url.getExpertSite(uid));
+				
+				uname = 
+						doc.getElementsByAttributeValue(HtmlHelper.CLASS, HtmlHelper.getExpertNameDivClass1()).get(0).
+						getElementsByAttributeValue(HtmlHelper.CLASS, HtmlHelper.getExpertNameDivClass2()).
+						get(0).child(0).child(0).text();
+				
+				Element tbl = 
+						doc.getElementsByAttributeValue(
+								HtmlHelper.CLASS, 
+								HtmlHelper.getExpertResultDivClass()).get(0).
+								getElementsByTag(HtmlHelper.TABLE).get(1).child(0);
+				
+				len2 = tbl.children().size();
+				
+				Element row = null;
+				Expert expert = null;
+				
+				cnt2 = len2 - 1;
+				for(cnt2 = len2 - 1; cnt2 >= 0; cnt2--){
+					row = tbl.child(cnt2);
+					
+					if(row.children().size() < 6 || row.child(5).children().size() == 0){
+						continue;
+					}
+					
+					expert = new Expert();
+					expert.setUid(uid);
+					expert.setUname(uname);
+					expert.setType(row.child(1).child(0).text());
+					expert.setDate(row.child(2).child(0).text());
+					expert.setHost(row.child(3).child(0).child(0).child(0).text());
+					expert.setGuest(row.child(3).child(0).child(0).child(2).text());
+					expert.setP(row.child(4).text());
+					String t = row.child(5).child(0).attr(HtmlHelper.SRC);
+					if(t.contains(HtmlHelper.getExpertWinImgName())){
+						expert.setResult(true);
+					}else if(t.contains(HtmlHelper.getExpertLostImgName())){
+						expert.setResult(false);
+					}
+					
+					expertList.add(expert);
 				}
 				
-				expertList.add(expert);
-			}
+				usedUid.add(uid);
 			
-			usedUid.add(uid);
-		
-		}
-		
+			}
+//		}catch(Exception e){
+//			System.out.println("error on " + uid);
+//			e.printStackTrace();
+//		}
 		return expertList;
 	}
 }
